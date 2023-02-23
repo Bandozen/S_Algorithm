3d8915be7ec79f74cb1ca42418fb7c273f296451
조찬익 <whcksdlr5909@gmail.com>
2023-02-24 01:21:10 +0900

    "탈주범 완료!"

--- 230223/SWEA_탈주범검거.py
+++ 230223/SWEA_탈주범검거.py
@@ -1,15 +1,17 @@
-from pprint import pprint
+
 # 터널 구조물
-dic = {1 : [(-1, 0), (1, 0), (0, -1), (0, 1)],
-       2 : [(-1, 0), (1, 0)],
-       3 : [(0, -1), (0, 1)],
-       4 : [(-1, 0), (0, 1)],
-       5 : [(1, 0), (0, 1)],
-       6 : [(1, 0), (0, -1)],
-       7 : [(-1, 0), (0, -1)]}
+dic = {1: [(-1, 0), (1, 0), (0, -1), (0, 1)],
+       2: [(-1, 0), (1, 0)],
+       3: [(0, -1), (0, 1)],
+       4: [(-1, 0), (0, 1)],
+       5: [(1, 0), (0, 1)],
+       6: [(1, 0), (0, -1)],
+       7: [(-1, 0), (0, -1)]}
+
 
 def is_valid(r, c):
     return 0 <= r < n and 0 <= c < m
+
 
 t = int(input())
 for tc in range(1, t+1):
@@ -24,12 +26,12 @@ for tc in range(1, t+1):
         for a, b in dic[tunnel[r][c]]:
             nr = r + a
             nc = c + b
-            if is_valid(nr, nc) and tunnel[nr][nc] != 0 and (-a, -b) in dic[tunnel[r][c]] and not visited[nr][nc]:
+            if is_valid(nr, nc) and tunnel[nr][nc] != 0 and (-a, -b) in dic[tunnel[nr][nc]] and not visited[nr][nc]:
                 q.append((nr, nc))
                 visited[nr][nc] = visited[r][c] + 1
-                if visited[nr][nc] <= l:
+                if visited[nr][nc] > l:
                     break
                 else:
-                    cnt+=1
+                    cnt += 1
 
     print(f'#{tc} {cnt}')
